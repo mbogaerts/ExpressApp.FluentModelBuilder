e97d3c0ed3c20f7abd0200363d3d14afd2d1f301
mbogaerts <Michael@3Way.be>
2017-02-23 08:58:17 +0100

    Added ListViewFilterAttribute Added RuleRequiredFieldAttribute Added RuleStringComparisonAttribute Added RuleUniqueValueAttribute Added RuleValueComparisonAttribute Added RuleCriteriaAttribute Added RuleObjectExistsAttribute

---
+++ FluentModelBuilder/ModelBuilder.ListViewFilter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DevExpress.ExpressApp.SystemModule;
+
+namespace ExpressApp.FluentModelBuilder.XAF
+{
+    public partial class ModelBuilder<T>
+    {
+        public ModelBuilder<T> HasListViewFilter(string id, string criteria)
+        {
+            return WithAttribute(new ListViewFilterAttribute(id, criteria));
+        }
+
+        public ModelBuilder<T> HasListViewFilter(string id, string criteria, bool isCurrentFilter)
+        {
+            return WithAttribute(new ListViewFilterAttribute(id, criteria, isCurrentFilter));
+        }
+
+        public ModelBuilder<T> HasListViewFilter(string id, string criteria, string caption)
+        {
+            return WithAttribute(new ListViewFilterAttribute(id, criteria, caption));
+        }
+
+        public ModelBuilder<T> HasListViewFilter(string id, string criteria, string caption, bool isCurrentFilter)
+        {
+            return WithAttribute(new ListViewFilterAttribute(id, criteria, caption, isCurrentFilter));
+        }
+
+        public ModelBuilder<T> HasListViewFilter(string id, string criteria, string caption, string description, bool isCurrentFilter)
+        {
+            return WithAttribute(new ListViewFilterAttribute(id, criteria, caption, description, isCurrentFilter));
+        }
+
+        public ModelBuilder<T> HasListViewFilter(string id, string criteria, string caption, string description, int index, bool isCurrentFilter)
+        {
+            return WithAttribute(new ListViewFilterAttribute(id, criteria, caption, description, index, isCurrentFilter));
+        }
+    }
+}
