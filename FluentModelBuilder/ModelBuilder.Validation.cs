5b7393c6b1570f160ea0f6b07df4db61e49bc5aa
mbogaerts <Michael@3Way.be>
2017-02-28 08:11:03 +0100

    fix: Incorretly moved HasRuleCriteria to property builder

--- FluentModelBuilder/ModelBuilder.Validation.cs
+++ FluentModelBuilder/ModelBuilder.Validation.cs
@@ -9,6 +9,38 @@ namespace ExpressApp.FluentModelBuilder.XAF
 {
     public partial class ModelBuilder<T>
     {
+        #region HasRuleCriteria
+        public ModelBuilder<T> HasRuleCriteria(string criteria)
+        {
+            return WithAttribute(new RuleCriteriaAttribute(criteria));
+        }
+
+        public ModelBuilder<T> HasRuleCriteria(DefaultContexts targetContext, string criteria)
+        {
+            return WithAttribute(new RuleCriteriaAttribute(targetContext, criteria));
+        }
+
+        public ModelBuilder<T> HasRuleCriteria(string id, DefaultContexts targetContext, string criteria)
+        {
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContext, criteria));
+        }
+
+        public ModelBuilder<T> HasRuleCriteria(string id, string targetContextIDs, string criteria)
+        {
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContextIDs, criteria));
+        }
+
+        public ModelBuilder<T> HasRuleCriteria(string id, DefaultContexts targetContext, string criteria, string messageTemplate)
+        {
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContext, criteria, messageTemplate));
+        }
+
+        public ModelBuilder<T> HasRuleCriteria(string id, string targetContextIDs, string criteria, string messageTemplate)
+        {
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContextIDs, criteria, messageTemplate));
+        }
+        #endregion
+
         #region HasRuleObjectExists
         public ModelBuilder<T> HasRuleObjectExists(string criteria)
         {
