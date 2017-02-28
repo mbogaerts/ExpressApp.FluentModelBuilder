59b6c770ed2c0cc9ad6af0dac3ea1bfa8e9b2d6d
mbogaerts <Michael@3Way.be>
2017-02-28 08:48:13 +0100

    Added invertresult to modelbuilder validation rules

--- FluentModelBuilder/ModelBuilder.Validation.cs
+++ FluentModelBuilder/ModelBuilder.Validation.cs
@@ -10,66 +10,66 @@ namespace ExpressApp.FluentModelBuilder.XAF
     public partial class ModelBuilder<T>
     {
         #region HasRuleCriteria
-        public ModelBuilder<T> HasRuleCriteria(string criteria)
+        public ModelBuilder<T> HasRuleCriteria(string criteria, bool invertResult = false)
         {
-            return WithAttribute(new RuleCriteriaAttribute(criteria));
+            return WithAttribute(new RuleCriteriaAttribute(criteria) { InvertResult = invertResult });
         }
 
-        public ModelBuilder<T> HasRuleCriteria(DefaultContexts targetContext, string criteria)
+        public ModelBuilder<T> HasRuleCriteria(DefaultContexts targetContext, string criteria, bool invertResult = false)
         {
-            return WithAttribute(new RuleCriteriaAttribute(targetContext, criteria));
+            return WithAttribute(new RuleCriteriaAttribute(targetContext, criteria) { InvertResult = invertResult });
         }
 
-        public ModelBuilder<T> HasRuleCriteria(string id, DefaultContexts targetContext, string criteria)
+        public ModelBuilder<T> HasRuleCriteria(string id, DefaultContexts targetContext, string criteria, bool invertResult = false)
         {
-            return WithAttribute(new RuleCriteriaAttribute(id, targetContext, criteria));
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContext, criteria) { InvertResult = invertResult });
         }
 
-        public ModelBuilder<T> HasRuleCriteria(string id, string targetContextIDs, string criteria)
+        public ModelBuilder<T> HasRuleCriteria(string id, string targetContextIDs, string criteria, bool invertResult = false)
         {
-            return WithAttribute(new RuleCriteriaAttribute(id, targetContextIDs, criteria));
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContextIDs, criteria) { InvertResult = invertResult });
         }
 
-        public ModelBuilder<T> HasRuleCriteria(string id, DefaultContexts targetContext, string criteria, string messageTemplate)
+        public ModelBuilder<T> HasRuleCriteria(string id, DefaultContexts targetContext, string criteria, string messageTemplate, bool invertResult = false)
         {
-            return WithAttribute(new RuleCriteriaAttribute(id, targetContext, criteria, messageTemplate));
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContext, criteria, messageTemplate) { InvertResult = invertResult });
         }
 
-        public ModelBuilder<T> HasRuleCriteria(string id, string targetContextIDs, string criteria, string messageTemplate)
+        public ModelBuilder<T> HasRuleCriteria(string id, string targetContextIDs, string criteria, string messageTemplate, bool invertResult = false)
         {
-            return WithAttribute(new RuleCriteriaAttribute(id, targetContextIDs, criteria, messageTemplate));
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContextIDs, criteria, messageTemplate) { InvertResult = invertResult });
         }
         #endregion
 
         #region HasRuleObjectExists
-        public ModelBuilder<T> HasRuleObjectExists(string criteria)
+        public ModelBuilder<T> HasRuleObjectExists(string criteria, bool invertResult = false)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(criteria));
+            return WithAttribute(new RuleObjectExistsAttribute(criteria) { InvertResult = invertResult });
         }
 
-        public ModelBuilder<T> HasRuleObjectExists(DefaultContexts targetContext, string criteria)
+        public ModelBuilder<T> HasRuleObjectExists(DefaultContexts targetContext, string criteria, bool invertResult = false)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(targetContext, criteria));
+            return WithAttribute(new RuleObjectExistsAttribute(targetContext, criteria) { InvertResult = invertResult });
         }
 
-        public ModelBuilder<T> HasRuleObjectExists(string id, DefaultContexts targetContext, string criteria)
+        public ModelBuilder<T> HasRuleObjectExists(string id, DefaultContexts targetContext, string criteria, bool invertResult = false)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(id, targetContext, criteria));
+            return WithAttribute(new RuleObjectExistsAttribute(id, targetContext, criteria) { InvertResult = invertResult });
         }
 
-        public ModelBuilder<T> HasRuleObjectExists(string id, string targetContextIDs, string criteria)
+        public ModelBuilder<T> HasRuleObjectExists(string id, string targetContextIDs, string criteria, bool invertResult = false)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(id, targetContextIDs, criteria));
+            return WithAttribute(new RuleObjectExistsAttribute(id, targetContextIDs, criteria) { InvertResult = invertResult });
         }
 
-        public ModelBuilder<T> HasRuleObjectExists(string id, DefaultContexts targetContext, string criteria, string messageTemplate)
+        public ModelBuilder<T> HasRuleObjectExists(string id, DefaultContexts targetContext, string criteria, string messageTemplate, bool invertResult = false)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(id, targetContext, criteria, messageTemplate));
+            return WithAttribute(new RuleObjectExistsAttribute(id, targetContext, criteria, messageTemplate) { InvertResult = invertResult });
         }
 
-        public ModelBuilder<T> HasRuleObjectExists(string id, string targetContextIDs, string criteria, string messageTemplate)
+        public ModelBuilder<T> HasRuleObjectExists(string id, string targetContextIDs, string criteria, string messageTemplate, bool invertResult = false)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(id, targetContextIDs, criteria, messageTemplate));
+            return WithAttribute(new RuleObjectExistsAttribute(id, targetContextIDs, criteria, messageTemplate) { InvertResult = invertResult });
         }
         #endregion
 
