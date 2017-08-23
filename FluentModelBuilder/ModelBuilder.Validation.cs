75e0eae603062443d2149161ff4b62c419e88781
mbogaerts <michael.bogaerts@belgoprocess.be>
2017-08-23 09:18:58 +0200

    Added SkipNullOrEmptyValues

--- FluentModelBuilder/ModelBuilder.Validation.cs
+++ FluentModelBuilder/ModelBuilder.Validation.cs
@@ -10,133 +10,133 @@ namespace ExpressApp.FluentModelBuilder.XAF
     public partial class ModelBuilder<T>
     {
         #region HasRuleCriteria
-        public ModelBuilder<T> HasRuleCriteria(string criteria, bool invertResult = false)
+        public ModelBuilder<T> HasRuleCriteria(string criteria, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleCriteriaAttribute(criteria) { InvertResult = invertResult });
+            return WithAttribute(new RuleCriteriaAttribute(criteria) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleCriteria(DefaultContexts targetContext, string criteria, bool invertResult = false)
+        public ModelBuilder<T> HasRuleCriteria(DefaultContexts targetContext, string criteria, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleCriteriaAttribute(targetContext, criteria) { InvertResult = invertResult });
+            return WithAttribute(new RuleCriteriaAttribute(targetContext, criteria) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleCriteria(string id, DefaultContexts targetContext, string criteria, bool invertResult = false)
+        public ModelBuilder<T> HasRuleCriteria(string id, DefaultContexts targetContext, string criteria, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleCriteriaAttribute(id, targetContext, criteria) { InvertResult = invertResult });
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContext, criteria) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleCriteria(string id, string targetContextIDs, string criteria, bool invertResult = false)
+        public ModelBuilder<T> HasRuleCriteria(string id, string targetContextIDs, string criteria, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleCriteriaAttribute(id, targetContextIDs, criteria) { InvertResult = invertResult });
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContextIDs, criteria) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleCriteria(string id, DefaultContexts targetContext, string criteria, string messageTemplate, bool invertResult = false)
+        public ModelBuilder<T> HasRuleCriteria(string id, DefaultContexts targetContext, string criteria, string messageTemplate, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleCriteriaAttribute(id, targetContext, criteria, messageTemplate) { InvertResult = invertResult });
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContext, criteria, messageTemplate) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleCriteria(string id, string targetContextIDs, string criteria, string messageTemplate, bool invertResult = false)
+        public ModelBuilder<T> HasRuleCriteria(string id, string targetContextIDs, string criteria, string messageTemplate, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleCriteriaAttribute(id, targetContextIDs, criteria, messageTemplate) { InvertResult = invertResult });
+            return WithAttribute(new RuleCriteriaAttribute(id, targetContextIDs, criteria, messageTemplate) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
         #endregion
 
         #region HasRuleObjectExists
-        public ModelBuilder<T> HasRuleObjectExists(string criteria, bool invertResult = false)
+        public ModelBuilder<T> HasRuleObjectExists(string criteria, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(criteria) { InvertResult = invertResult });
+            return WithAttribute(new RuleObjectExistsAttribute(criteria) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleObjectExists(DefaultContexts targetContext, string criteria, bool invertResult = false)
+        public ModelBuilder<T> HasRuleObjectExists(DefaultContexts targetContext, string criteria, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(targetContext, criteria) { InvertResult = invertResult });
+            return WithAttribute(new RuleObjectExistsAttribute(targetContext, criteria) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleObjectExists(string id, DefaultContexts targetContext, string criteria, bool invertResult = false)
+        public ModelBuilder<T> HasRuleObjectExists(string id, DefaultContexts targetContext, string criteria, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(id, targetContext, criteria) { InvertResult = invertResult });
+            return WithAttribute(new RuleObjectExistsAttribute(id, targetContext, criteria) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleObjectExists(string id, string targetContextIDs, string criteria, bool invertResult = false)
+        public ModelBuilder<T> HasRuleObjectExists(string id, string targetContextIDs, string criteria, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(id, targetContextIDs, criteria) { InvertResult = invertResult });
+            return WithAttribute(new RuleObjectExistsAttribute(id, targetContextIDs, criteria) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleObjectExists(string id, DefaultContexts targetContext, string criteria, string messageTemplate, bool invertResult = false)
+        public ModelBuilder<T> HasRuleObjectExists(string id, DefaultContexts targetContext, string criteria, string messageTemplate, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(id, targetContext, criteria, messageTemplate) { InvertResult = invertResult });
+            return WithAttribute(new RuleObjectExistsAttribute(id, targetContext, criteria, messageTemplate) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleObjectExists(string id, string targetContextIDs, string criteria, string messageTemplate, bool invertResult = false)
+        public ModelBuilder<T> HasRuleObjectExists(string id, string targetContextIDs, string criteria, string messageTemplate, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleObjectExistsAttribute(id, targetContextIDs, criteria, messageTemplate) { InvertResult = invertResult });
+            return WithAttribute(new RuleObjectExistsAttribute(id, targetContextIDs, criteria, messageTemplate) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
         #endregion
 
         #region RuleIsReferenced
-        public ModelBuilder<T> HasRuleIsReferenced(Type looksFor, string refrencePropertyName, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(Type looksFor, string refrencePropertyName, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(looksFor, refrencePropertyName) { InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(looksFor, refrencePropertyName) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(DefaultContexts targetContext, Type looksFor, string refrencePropertyName, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(DefaultContexts targetContext, Type looksFor, string refrencePropertyName, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(targetContext, looksFor, refrencePropertyName) { InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(targetContext, looksFor, refrencePropertyName) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(string id, DefaultContexts targetContext, Type looksFor, string refrencePropertyName, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(string id, DefaultContexts targetContext, Type looksFor, string refrencePropertyName, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(id, targetContext, looksFor, refrencePropertyName) { InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(id, targetContext, looksFor, refrencePropertyName) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(string id, string targetContextIDs, Type looksFor, string refrencePropertyName, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(string id, string targetContextIDs, Type looksFor, string refrencePropertyName, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(id, targetContextIDs, looksFor, refrencePropertyName) { InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(id, targetContextIDs, looksFor, refrencePropertyName) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(string id, DefaultContexts targetContext, Type looksFor, string refrencePropertyName, string messageTemplate, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(string id, DefaultContexts targetContext, Type looksFor, string refrencePropertyName, string messageTemplate, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(id, targetContext, looksFor, refrencePropertyName, messageTemplate) { InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(id, targetContext, looksFor, refrencePropertyName, messageTemplate) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(string id, string targetContextIDs, Type looksFor, string refrencePropertyName, string messageTemplate, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(string id, string targetContextIDs, Type looksFor, string refrencePropertyName, string messageTemplate, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(id, targetContextIDs, looksFor, refrencePropertyName, messageTemplate) { InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(id, targetContextIDs, looksFor, refrencePropertyName, messageTemplate) { InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(Type looksFor, string refrencePropertyName, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(Type looksFor, string refrencePropertyName, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(looksFor, refrencePropertyName) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior,  InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(looksFor, refrencePropertyName) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior,  InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(DefaultContexts targetContext, Type looksFor, string refrencePropertyName, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(DefaultContexts targetContext, Type looksFor, string refrencePropertyName, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(targetContext, looksFor, refrencePropertyName) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(targetContext, looksFor, refrencePropertyName) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(DefaultContexts targetContext, Type looksFor, string refrencePropertyName, CriteriaEvaluationBehavior criteriaEvaluationBehavior, string messageTemplate, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(DefaultContexts targetContext, Type looksFor, string refrencePropertyName, CriteriaEvaluationBehavior criteriaEvaluationBehavior, string messageTemplate, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute("", targetContext, looksFor, refrencePropertyName, messageTemplate) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute("", targetContext, looksFor, refrencePropertyName, messageTemplate) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(string id, DefaultContexts targetContext, Type looksFor, string refrencePropertyName, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(string id, DefaultContexts targetContext, Type looksFor, string refrencePropertyName, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(id, targetContext, looksFor, refrencePropertyName) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(id, targetContext, looksFor, refrencePropertyName) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(string id, string targetContextIDs, Type looksFor, string refrencePropertyName, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(string id, string targetContextIDs, Type looksFor, string refrencePropertyName, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(id, targetContextIDs, looksFor, refrencePropertyName) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(id, targetContextIDs, looksFor, refrencePropertyName) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(string id, DefaultContexts targetContext, Type looksFor, string refrencePropertyName, string messageTemplate, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(string id, DefaultContexts targetContext, Type looksFor, string refrencePropertyName, string messageTemplate, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(id, targetContext, looksFor, refrencePropertyName, messageTemplate) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(id, targetContext, looksFor, refrencePropertyName, messageTemplate) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
 
-        public ModelBuilder<T> HasRuleIsReferenced(string id, string targetContextIDs, Type looksFor, string refrencePropertyName, string messageTemplate, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false)
+        public ModelBuilder<T> HasRuleIsReferenced(string id, string targetContextIDs, Type looksFor, string refrencePropertyName, string messageTemplate, CriteriaEvaluationBehavior criteriaEvaluationBehavior, bool invertResult = false, bool skipNullOrEmptyValues = true)
         {
-            return WithAttribute(new RuleIsReferencedAttribute(id, targetContextIDs, looksFor, refrencePropertyName, messageTemplate) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult });
+            return WithAttribute(new RuleIsReferencedAttribute(id, targetContextIDs, looksFor, refrencePropertyName, messageTemplate) { CriteriaEvaluationBehavior = criteriaEvaluationBehavior, InvertResult = invertResult, SkipNullOrEmptyValues = skipNullOrEmptyValues});
         }
         
         #endregion
